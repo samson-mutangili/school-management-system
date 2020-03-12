fc222f52e63315a8eb06132f4a01af6c21ad3f5c
root <root@localhost.localdomain>
2020-03-12 11:08:27 -0400

    ateachers functionality

---
+++ public/Login_v8/images/School Fees Payment System In PHP With Source Code _ Source Code & Projects_files/blueprint.js
@@ -0,0 +1,227 @@
+window.blueprint = {
+	"width": 300,
+	"height": 600,
+	"images": [
+		"txt_1.png",
+		"txt_2.png",
+		"txt_3.png",
+		"cta_1.png",
+		"ctaHover_1.png",
+		"hand.png",
+		"logo_vimeo.png"
+	],
+	"elements": {
+		"stage": {
+			"parent": "banner",
+			"id": "stage",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 600
+		},
+		"bg": {
+			"parent": "banner",
+			"id": "bg",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 600
+		},
+		"slider": {
+			"parent": "banner",
+			"id": "slider",
+			"retina": false,
+			"left": 188,
+			"top": -114,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 25,
+			"height": 25
+		},
+		"txt_1": {
+			"parent": "banner",
+			"id": "txt_1",
+			"retina": true,
+			"left": -13,
+			"top": 78,
+			"safeArea": 45,
+			"lineHeight": 33.480000000000004,
+			"backgroundImage": "txt_1.png",
+			"width": 321,
+			"height": 217
+		},
+		"txt_2": {
+			"parent": "banner",
+			"id": "txt_2",
+			"retina": true,
+			"left": -13,
+			"top": 78.36,
+			"safeArea": 45,
+			"lineHeight": 36.38,
+			"backgroundImage": "txt_2.png",
+			"width": 312,
+			"height": 155
+		},
+		"txt_3": {
+			"parent": "banner",
+			"id": "txt_3",
+			"retina": true,
+			"left": 7,
+			"top": 179,
+			"safeArea": 25,
+			"lineHeight": 26,
+			"backgroundImage": "txt_3.png",
+			"width": 246,
+			"height": 71
+		},
+		"cta": {
+			"parent": "banner",
+			"id": "cta",
+			"retina": true,
+			"left": 32,
+			"top": 295.25,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"cta_1": {
+			"parent": "cta",
+			"id": "cta_1",
+			"retina": true,
+			"backgroundImage": "cta_1.png"
+		},
+		"ctaHover": {
+			"parent": "banner",
+			"id": "ctaHover",
+			"retina": true,
+			"left": 163,
+			"top": 377.25,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"ctaHover_1": {
+			"parent": "ctaHover",
+			"id": "ctaHover_1",
+			"retina": true,
+			"backgroundImage": "ctaHover_1.png"
+		},
+		"hand": {
+			"parent": "banner",
+			"id": "hand",
+			"retina": true,
+			"left": 355,
+			"top": 91,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "hand.png",
+			"width": 51,
+			"height": 62
+		},
+		"hand_pos1": {
+			"parent": "banner",
+			"id": "hand_pos1",
+			"retina": false,
+			"left": 207,
+			"top": 68,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 52,
+			"height": 62
+		},
+		"hand_pos2": {
+			"parent": "banner",
+			"id": "hand_pos2",
+			"retina": false,
+			"left": 122,
+			"top": -78,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 52,
+			"height": 63
+		},
+		"logo_vimeo": {
+			"parent": "banner",
+			"id": "logo_vimeo",
+			"retina": true,
+			"left": 32,
+			"top": 50,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_vimeo.png",
+			"width": 103,
+			"height": 30
+		},
+		"top": {
+			"parent": "banner",
+			"id": "top",
+			"retina": false,
+			"left": 0,
+			"top": -10,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 10
+		},
+		"bottom": {
+			"parent": "banner",
+			"id": "bottom",
+			"retina": false,
+			"left": 0,
+			"top": 600,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 300,
+			"height": 10
+		},
+		"left": {
+			"parent": "banner",
+			"id": "left",
+			"retina": false,
+			"left": -10,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 600
+		},
+		"right": {
+			"parent": "banner",
+			"id": "right",
+			"retina": false,
+			"left": 300,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 600
+		}
+	},
+	"settings": {
+		"filename": "Q3-2019_EN_mid-funnel_marketer_custom-color_2_learn-more_300x600_HTML5",
+		"market": "EN",
+		"campaign": "Q3-2019",
+		"funnel": "mid-funnel",
+		"segment": "marketer",
+		"feature": "custom-color",
+		"copyVariation": "2",
+		"size": "300x600",
+		"marketIndex": "1",
+		"id": "marketercustom-color2",
+		"copy": {
+			"copy1": "Get matchy-matchy with your brandy brand.",
+			"copy2": "Customizable Player.",
+			"copy3": "A Vimeo Feature",
+			"copy4": "Learn More",
+			"copy5": ""
+		},
+		"fileType": "jpg",
+		"fallback": true,
+		"HtmlfileType": "html",
+		"backgroundColor": "#e59fb9"
+	}
+};
